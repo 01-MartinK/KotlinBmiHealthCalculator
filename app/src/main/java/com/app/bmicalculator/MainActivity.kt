3c3a4f84ea5a799a0977e00fcdb098d6e4097154
MK <thedeadgamer101@gmail.com>
2022-07-11 13:20:13 +0300

    Bmi calculation and visualization with error management

--- app/src/main/java/com/app/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/app/bmicalculator/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.app.bmicalculator
 
+import android.app.usage.UsageEvents
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -13,27 +21,45 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+
+        val calcButton: Button = findViewById(R.id.CalculateBmiButton)
+        calcButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val HeightText = findViewById<EditText>(R.id.HeightCalculationField).text.toString()
+                val WeightText = findViewById<EditText>(R.id.WeightCalculationField).text.toString()
+
+                var AnswerText = findViewById<TextView>(R.id.BmiCalcAnswerText)
+
+                var bmi = calculateBmi(convertStringToFloat(HeightText), convertStringToFloat(WeightText))
+
+                AnswerText.text = Math.round(bmi).toString()
+            }
+        })
     }
 
-    fun CalculateBmi(height: Float, weight: Float): Float {
-        var bmi = weight / (height * height);
+    fun calculateBmi(height: Float, weight: Float): Float {
+        var bmi = weight / (height / 100 * height / 100);
         return bmi;
     }
 
-    fun ConvertStringToFloat(input: String): Float {
-        return if (CheckForFloat(input)) {
-            var b: Float = ConvertStringToFloat(input);
+    private fun convertStringToFloat(input: String): Float {
+        var errorView: TextView = findViewById(R.id.ErrorMessage);
+
+        return if (checkForFloat(input)) {
+            errorView.visibility = TextView.INVISIBLE;
+            var b: Float = input.toFloat()
             b;
         }else {
+            errorView.visibility = TextView.VISIBLE;
             0.0f;
         }
     }
 
-    fun CheckForFloat(input: String): Boolean {
+    private fun checkForFloat(input: String): Boolean {
         try {
-            ConvertStringToFloat(input)
-        } catch (e: NullPointerException) {
-            println("failed because contains letters")
+            input.toFloat()
+        } catch (e: NumberFormatException) {
+            println("failed because contains letters $e")
             return false;
         }
 

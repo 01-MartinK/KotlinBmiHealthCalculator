0a0b9257a027de6ee87abe53bacf893026a5bfe1
MK <thedeadgamer101@gmail.com>
2022-07-11 12:33:55 +0300

    Initial commit and basic XML/main file setup

---
+++ app/src/main/java/com/app/bmicalculator/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.app.bmicalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        setContentView(R.layout.activity_main)
+    }
+
+    fun checkForFloat(input: String): Boolean {
+        return false;
+    }
+}
